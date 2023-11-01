17cf1bef5c7df2cb71986894082e41b6d1d22a47
Rafael Souto <rafaelsouto1326@gmail.com>
2023-10-31 22:13:53 -0300

    ✨ Implements methods, 'GET, PUT and Delete'.

--- Back/hubNotes/hubNotes/src/main/java/com/PIappHubNotes/hubNotes/controller/NotaController.java
+++ Back/hubNotes/hubNotes/src/main/java/com/PIappHubNotes/hubNotes/controller/NotaController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -20,5 +19,31 @@ public class NotaController {
     @ResponseStatus(HttpStatus.CREATED)
     public Nota adicionarNota(@RequestBody Nota nota) {
         return notaRepository.save(nota);
+    }
+
+    @GetMapping
+    public List<Nota> listarNotas() {
+        return notaRepository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Nota> atualizarNota(@PathVariable String id, @RequestBody Nota nota) {
+        Nota updatedNota = notaRepository.findById(id)
+                .map(n -> {
+                    n.setTitle(nota.getTitle());
+                    n.setBody(nota.getBody());
+                    return notaRepository.save(n);
+                })
+                .orElseGet(() -> {
+                    nota.setId(id);
+                    return notaRepository.save(nota);
+                });
+        return new ResponseEntity<>(updatedNota, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirNota(@PathVariable String id) {
+        notaRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
