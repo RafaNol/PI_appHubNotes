a13b6bff4c1527a23c849991ecfabec1e81b20aa
rypegrowth <programapedror@gmail.com>
2023-11-10 15:07:13 -0300

    Adicionando e ativando auditing para pegar timestamp

--- Back/hubNotes/hubNotes/src/main/java/com/PIappHubNotes/hubNotes/HubNotesApplication.java
+++ Back/hubNotes/hubNotes/src/main/java/com/PIappHubNotes/hubNotes/HubNotesApplication.java
@@ -2,8 +2,10 @@ package com.PIappHubNotes.hubNotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class HubNotesApplication {
 
 	public static void main(String[] args) {
